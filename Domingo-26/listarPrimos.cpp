e74b9db7d8244f48cd4b98d0b9f3a5dcb4240b57
Jomaaph <jomaaph@outlook.com>
2026-07-27 00:25:48 +0100

    6. Listar todos os números primos até N.

--- Domingo-26/listarPrimos.cpp
+++ Domingo-26/listarPrimos.cpp
@@ -1,5 +1,41 @@
 #include <iostream>
 
-int main(){
-    
+int main()
+{
+    using namespace std;
+    int n;
+    n = 100;
+    int lista[n];
+
+    lista[0] = 0;
+    lista[1] = 0;
+
+    // listar todos como primos
+    // 1. listar todos os n 2 ate n como primos .
+    // 2 listar dos multiplos como nao primos
+    for (int i = 2; i < n; i++)
+    {
+        lista[i] = 1;
+    }
+
+    // mostrar lista
+    for (int i = 2; i * i <= n; i++)
+    {
+        for (int p = i+i; p < n; p += i)
+        {
+            lista[p] = 0;
+        }
+    }
+
+
+    for (int i = 2; i < n; i++)
+    {
+        /* code */
+        if(lista[i] == 1)
+        {
+            cout << i;
+            cout << " ";
+        }
+    }
+    cout << "\n";
 }
